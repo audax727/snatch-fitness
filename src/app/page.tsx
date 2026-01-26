6a7bdb0ff245115c6478e9debee97cbd2ab6b103
Audax <audax727@gmail.com>
2026-01-26 16:11:43 +0000

    remove meet your coaches section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import HeroSection from '@/components/sections/hero-section';
 import AboutSection from '@/components/sections/about-section';
 import FacilitiesSection from '@/components/sections/facilities-section';
 import MembershipSection from '@/components/sections/membership-section';
-// import TestimonialsSection from '@/components/sections/testimonials-section';
-import TrainersSection from '@/components/sections/trainers-section';
 import LocationSection from '@/components/sections/location-section';
 import CtaSection from '@/components/sections/cta-section';
 
@@ -19,8 +17,6 @@ export default function Home() {
         <AboutSection />
         <FacilitiesSection />
         <MembershipSection />
-        {/* <TestimonialsSection /> */}
-        <TrainersSection />
         <LocationSection />
         <CtaSection />
       </main>
